5e2a943e65cef0cbe300e1ae7bfae68934f135c1
Sbrawat <xbrawat@protonmail.com>
2026-05-08 11:12:28 +0530

    feat: wrap App component with SessionProvider and WeatherProvider for context management

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { SessionProvider } from './context/SessionContext.jsx';
+import { WeatherProvider } from './context/WeatherContext.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SessionProvider>
+        <WeatherProvider>
+            <App />
+        </WeatherProvider>
+    </SessionProvider>
+  </React.StrictMode>
+);
